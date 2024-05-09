40a2e16f16017f4a0704313bf031d1163cddda7f
Pedro Ferreira <pedrodif@icloud.com>
2024-05-09 14:28:23 -0300

    feat/cria e armazena postagens no localStorage, corrige html

---
+++ codigo/ASSETS/js/forum.js
@@ -0,0 +1,71 @@
+
+// localStorage.clear();
+
+var id = 1
+
+const usuario = {
+    level: 'Bronze',
+    profissao: 'Estudante',
+    nome: 'Pedro da Silva',
+    data: new Date(),
+}
+
+function determinaId() {
+    id++
+}
+
+
+function salvaPostagens(dados) {
+    localStorage.setItem('db', JSON.stringify(dados))
+}
+
+function consultaPostagens() {
+    let dados = localStorage.getItem('db')
+    let postagens = {}
+
+    if (dados) {
+        postagens = JSON.parse(dados)
+        id = postagens.data.at(-1).id
+    } else {
+        postagens = {
+            data: [
+                {
+                    id,
+                    ...usuario,
+                    conteudo: 'Lorem ipsum dolor sit amet consectetur. Aenean enim diam ut donec consequat arcu habitant. Aliquam elit habitant duis.'
+                }
+            ]
+        }
+    }
+
+    return postagens
+}
+
+function criaPostagem() {
+    const inputData = document.querySelector('#inputEntrada').value
+
+    let postagens = consultaPostagens()
+    determinaId()
+
+    postagens.data.push({
+        id,
+        ...usuario,
+        conteudo: inputData,
+    })
+
+    salvaPostagens(postagens)
+}
+
+
+function imprimePostagens() {
+    console.log(inputData.value)
+
+
+    inputData.addEventListener('change', (e) => {
+        console.log(e.target.value)
+    })
+}
+
+// document.querySelector('#btnPublicar').addEventListener('click', imprimeValor)
+
+document.querySelector('#btnPublicar').addEventListener('click', criaPostagem)
