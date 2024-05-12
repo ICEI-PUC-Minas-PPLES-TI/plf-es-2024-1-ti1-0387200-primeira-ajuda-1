5acf4059eed5adfdefd833997d1c47018c830911
Pedro Ferreira <pedrodif@icloud.com>
2024-05-12 17:31:41 -0300

    refactor: :art: refatora função para formatar data com hora

--- codigo/ASSETS/js/forum/utils.js
+++ codigo/ASSETS/js/forum/utils.js
@@ -2,23 +2,17 @@ export const USUARIO = {
     level: 'Bronze',
     profissao: 'Estudante',
     nome: 'Pedro da Silva',
-    data: dataFormatada(),
+    data: formatDateWithTime(new Date()),
 }
-
-function dataFormatada() {
-    let data = new Date(),
-        dia = data.getDate().toString().padStart(2, '0'),
-        mes = (data.getMonth() + 1).toString().padStart(2, '0'),
-        ano = data.getFullYear();
-
-    return dia + "/" + mes + "/" + ano;
-}
-
 
 export const REMOVE_POSTAGEM = {
     titulo: "Deseja excluir essa postagem?",
     sucesso: "Sua postagem será excluída.",
     cancelado: "Ação cancelada!",
+}
+
+function formatDateWithTime(dateString) {
+    return dateString.toLocaleString().replace(',', '')
 }
 
 export function confirmaAcao(variante) {
