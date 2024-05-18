64396f9ac9217d942738eca7b1f0247adbdf0fb2
NateAlves <oliveiraalvesnatan@gmail.com>
2024-05-17 21:28:05 -0300

    Atualização do js

--- main.js
+++ main.js
@@ -1,13 +1,35 @@
+// Função para ser chamada quando o formulário for enviado
 function submitForm() {
-    const kitName = document.getElementById('kit-name').value;
-    alert('Nome do Kit é ' + kitName);
-    function submitForm() {
-        const kitName = document.getElementById('kit-name').value;
-        const itemsSelect = document.getElementById('items');
-        const selectedItems = Array.from(itemsSelect.selectedOptions).map(option => option.value);
-    
-        alert('Nome do Kit é ' + kitName);
-        alert('Itens selecionados: ' + selectedItems.join(', '));
+    // Obter o nome do kit digitado
+    var kitName = document.getElementById('kit-name').value;
+
+    // Obter os itens selecionados no select
+    var selectedItems = [];
+    var select = document.getElementById('items');
+    for (var i = 0; i < select.options.length; i++) {
+        if (select.options[i].selected) {
+            selectedItems.push(select.options[i].value);
+        }
     }
-    
+
+    // Criar um objeto para representar o kit
+    var kit = {
+        kitName: kitName,
+        selectedItems: selectedItems
+    };
+
+    // Verificar se já existe algum dado armazenado no localStorage
+    var kits = JSON.parse(localStorage.getItem('kits')) || [];
+
+    // Adicionar o novo kit ao array de kits
+    kits.push(kit);
+
+    // Armazenar o array de kits atualizado no localStorage
+    localStorage.setItem('kits', JSON.stringify(kits));
+
+    // Esvaziar o campo de texto
+    document.getElementById('kit-name').value = "";
+
+    // Exibir uma mensagem de confirmação
+    alert('Kit criado com sucesso e armazenado no localStorage!');
 }
