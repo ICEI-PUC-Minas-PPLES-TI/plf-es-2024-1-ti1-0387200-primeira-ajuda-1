fafa6d7d6348ddab4a3eca481f2b60e05a5fa6a9
Pedro Ferreira <pedrodif@icloud.com>
2024-06-20 15:53:38 -0300

    adiciona as funções createPostagem, updatePostagem, deletePostagem

--- codigo/assets/services/forumService.js
+++ codigo/assets/services/forumService.js
@@ -8,4 +8,32 @@ export class ForumService {
         return resposta.json()
     }
 
+    async createPostagem(postagem) {
+        const resposta = await fetch(this.urlBase, {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(postagem)
+        })
+        return resposta.json()
+    }
+
+    async updatePostagem(id, postagem) {
+        const resposta = await fetch(`${this.urlBase}/${id}`, {
+            method: 'PUT',
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(postagem)
+        })
+        return resposta.json()
+    }
+
+    async deletePostagem(id) {
+        const resposta = await fetch(`${this.urlBase}/${id}`, {
+            method: 'DELETE',
+        })
+        return resposta.json()
+    }
 }
