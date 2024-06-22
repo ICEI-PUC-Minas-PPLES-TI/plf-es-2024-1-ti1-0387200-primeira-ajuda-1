a4e72ea9047794f6da01617c0ed0986738d41a82
Pedro Ferreira <pedrodif@icloud.com>
2024-06-22 15:38:19 -0300

    refactor/cria pasta castroQuestoes dentro da pasta quiz

---
+++ codigo/assets/js/quiz/cadastroQuestoes/main.js
@@ -0,0 +1,80 @@
+import { QuizService } from "../../services/quizService.js";
+
+const quizService = new QuizService()
+
+function getNextID() {
+    let currentID = parseInt(localStorage.getItem('currentID')) || 0;
+    currentID++;
+    localStorage.setItem('currentID', currentID);
+    return currentID;
+}
+
+function SalvaDados(dados) {
+    // Obter as perguntas existentes do localStorage
+    let perguntasExistentes = JSON.parse(localStorage.getItem('CadastroPerguntas')) || [];
+    // Adicionar a nova pergunta às perguntas existentes
+    perguntasExistentes.push(dados);
+    // Salvar todas as perguntas no localStorage
+    localStorage.setItem('CadastroPerguntas', JSON.stringify(perguntasExistentes));
+}
+
+function ClearInputs() {
+    document.getElementById("inputquestion").value = "";
+    document.getElementById("option1").value = "";
+    document.getElementById("option2").value = "";
+    document.getElementById("option3").value = "";
+    document.getElementById("option4").value = "";
+    document.getElementById("answer").value = "";
+}
+
+async function SalvarPergunta() {
+    // Coletar Dados da pergunta
+    let Qpergunta = document.getElementById("inputquestion").value;
+    let Qoption1 = document.getElementById("option1").value;
+    let Qoption2 = document.getElementById("option2").value;
+    let Qoption3 = document.getElementById("option3").value;
+    let Qoption4 = document.getElementById("option4").value;
+    let Qresposta = document.getElementById("answer").value;
+
+    // Verificar se todos os campos estão preenchidos
+    if (!Qpergunta || !Qoption1 || !Qoption2 || !Qoption3 || !Qoption4 || !Qresposta) {
+        alert("Por favor, preencha todos os campos antes de salvar.");
+        return; // Não continua com o salvamento
+    }
+
+    // Verificar se a resposta é uma das alternativas
+    if (Qresposta !== Qoption1 && Qresposta !== Qoption2 && Qresposta !== Qoption3 && Qresposta !== Qoption4) {
+        alert("A resposta deve ser igual a uma das alternativas.");
+        return; // Não continua com o salvamento
+    }
+
+    // Verificar se há alternativas duplicadas
+    let alternativas = [Qoption1, Qoption2, Qoption3, Qoption4];
+    let alternativasSet = new Set(alternativas);
+    if (alternativasSet.size !== alternativas.length) {
+        alert("As alternativas devem ser únicas.");
+        return; // Não continua com o salvamento
+    }
+
+    // Formato dos dados com ID
+    let novaPergunta = {
+        id: getNextID(),
+        pergunta: Qpergunta,
+        alternativa1: Qoption1,
+        alternativa2: Qoption2,
+        alternativa3: Qoption3,
+        alternativa4: Qoption4,
+        resposta: Qresposta
+    }
+
+    // Adição do objeto ao Local Storage
+
+    // SalvaDados(novaPergunta);
+    const resposta = await quizService.createPergunta(novaPergunta);
+    if (resposta) {
+        // Alerta dizendo que a pergunta foi salva
+        alert("A pergunta foi salva!");
+        // Chamada da função que limpa os inputs
+        ClearInputs();
+    }
+}
